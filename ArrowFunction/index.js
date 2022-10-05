18f2f935f671a01ede15e23255f1225a141dac6e
mhamds <113810218+mhamds@users.noreply.github.com>
2022-10-04 21:08:56 -0400

    Revert "Update index.js"
    This reverts commit 410f267c77cd26857856ab59641658bd5ff183b8.

--- ArrowFunction/index.js
+++ ArrowFunction/index.js
@@ -1,63 +1,16 @@
-// var hello;
+var hello;
 
-// //Arrow function
+//Arrow function
 
-// hello = () => {
-// document.getElementById("demo").innerHTML += "Hello 1";
-// }
-
-
-// //A button object calls the function:
-// document.getElementById("btn1").addEventListener("click", hello);
-
-// //A button object calls the function:
-// document.getElementById("btn2").addEventListener(
-//     "click",
-//      () => {
-//     document.getElementById("demo").innerHTML += "Hello 2";
-//     }
-//     );    
-
-//     //Call back function
-
-// var numbers = [1,2,3,4];
-// const squares = numbers.map(n => n * n);
-// document.write(squares);
-
-// var evens = new Array();
-// numbers.forEach(n => {
-//     if (n % 2 === 0) {
-//         evens.push(n);
-//     }
-// });
-// document.write('<br>');
-// document.write(evens);
+hello = () => {
+document.getElementById("demo").innerHTML += "Hello 1";
+}
 
 
-const author = {
-    fullName: "Bob Alice",
-    books: ["Book 01","Book 02","Book 03"],
-    
-    printBooks() {
-      this.books.forEach(book => document.write(book + ' by ' +this.fullName + '<br>'));
-    },
+//A button object calls the function:
+document.getElementById("btn1").addEventListener("click", hello);
 
-    printBooks1() {
-      function printValue(book) {      
-         document.write(book + ' by ' +this.fullName + '<br>'); // Undefined expected for fullname
-      }
-
-      this.books.forEach(printValue);
-    },
-    
-    printBooks2() {
-      this.books.forEach(function(book){
-        document.write(book + ' by ' +this.fullName + '<br>'); // Undefined expected for fullname
-      });
-    }
-};
-
-
-author.printBooks();
-author.printBooks1();
-author.printBooks2();
+//A button object calls the function:
+document.getElementById("btn2").addEventListener("click", () => {
+    document.getElementById("demo").innerHTML += "Hello 2";
+    });    
