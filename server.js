a6f68b48fc3b9888be8df00e165b69071c78dcaf
mhamds <113810218+mhamds@users.noreply.github.com>
2022-10-06 20:56:56 -0400

    Simple Express App

--- server.js
+++ server.js
@@ -1,49 +1,14 @@
-// const hello = require('./hello')
+const express = require('express')
+const app = express()
+const port = 3000
 
-// hello.sayHello();
+// Route
 
-// const lib = require('./lib');
-// console.log( lib.halfOf(10));
-// console.log( lib.multiply(10, 2));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-//Node
-const http = require('http');
-
-// http.createServer((req, res) => {
-// res.writeHead(200, 
-//     {
-
-//     'Content-Type': 'test/plain'
-//     });
-// res.end('Hello World');
-// }).listen(3000);
-
-// console.log('Server running at http://localhost:3000/');
-
-// Connect 1 
-const connect = require('connect');
-const app = connect();
-app.listen(3000);
-
-function logger(req, res, next){
-    console.log(req.method, req.url);
-    next();
-}
-
-function helloWorld(req, res, next) {
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello World');
-};
-
-function goodbye(req, res, next) {
-    res.setHeader('Content-type', 'text/plain');
-    res.end('Good bye!');
-
-};
-    app.use(logger);
-    app.use('/hello', helloWorld);
-    app.use('/goodbye', goodbye);
-
-console.log('Server running at http://localhost:3000/');
-
-
+// Starts the server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
